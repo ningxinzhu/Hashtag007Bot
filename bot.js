0902277174e7ebef02403bae663f252a193ddb40
Ningxin Zhu <ningxinzhu95@gmail.com>
2017-01-01 15:39:36 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
+// This is the URL of a search for the latest tweets with James Bond-related hashtag.
 var bondSearch = {q: "#JamesBond, #007", count: 10, result_type: "recent"}; 
 
 // This function finds the latest tweet with the #JamesBond and #007 hashtags, and retweets it.
@@ -36,37 +36,4 @@ function retweetLatest() {
 	});
 }
 
-// find a random tweet and 'favorite' it
-var favoriteTweet = function(){
-  var params = {
-      q: '#JamesBond, #007',  // REQUIRED
-      result_type: 'recent',
-      lang: 'en'
-  }
-  // for more parametes, see: https://dev.twitter.com/rest/reference
-
-  // find the tweet
-  T.get('search/tweets', params, function(err,data){
-
-    // find tweets
-    var tweet = data.statuses;
-    var randomTweet = ranDom(tweet);   // pick a random tweet
-
-    // if random tweet exists
-    if(typeof randomTweet != 'undefined'){
-      // Tell TWITTER to 'favorite'
-      T.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
-        // if there was an error while 'favorite'
-        if(err){
-          console.log('CANNOT BE FAVORITE... Error');
-        }
-        else{
-          console.log('FAVORITED... Success!!!');
-        }
-      });
-    }
-  });
-}
-
 retweetLatest();
-favoriteTweet();
