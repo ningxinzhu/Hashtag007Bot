818af804be1400e8abaa96cd6ab2000cd756c0f0
Ningxin Zhu <ningxinzhu95@gmail.com>
2017-01-01 14:35:29 -0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+// Our Twitter library
+var Twit = require('twit');
+
+// We need to include our configuration file
+var T = new Twit(require('./config.js'));
+
+// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
+var bondSearch = {q: "#JamesBond, #007", count: 10, result_type: "recent"}; 
+
+// This function finds the latest tweet with the #JamesBond and #007 hashtags, and retweets it.
+function retweetLatest() {
+	T.get('search/tweets', bondSearch, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetId = data.statuses[0].id_str;
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
+// Try to retweet something as soon as we run the program...
+retweetLatest();
+// ...and then every five minutes
+setInterval(retweetLatest, 300000);
