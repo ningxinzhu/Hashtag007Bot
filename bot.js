c048ea0ed255eb9bf8b2b65f1fd8a373eb97e676
Ningxin Zhu <ningxinzhu95@gmail.com>
2017-01-01 15:26:47 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,4 +36,37 @@ function retweetLatest() {
 	});
 }
 
+// find a random tweet and 'favorite' it
+var favoriteTweet = function(){
+  var params = {
+      q: '#JamesBond, #007',  // REQUIRED
+      result_type: 'recent',
+      lang: 'en'
+  }
+  // for more parametes, see: https://dev.twitter.com/rest/reference
+
+  // find the tweet
+  T.get('search/tweets', params, function(err,data){
+
+    // find tweets
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet);   // pick a random tweet
+
+    // if random tweet exists
+    if(typeof randomTweet != 'undefined'){
+      // Tell TWITTER to 'favorite'
+      T.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+        // if there was an error while 'favorite'
+        if(err){
+          console.log('CANNOT BE FAVORITE... Error');
+        }
+        else{
+          console.log('FAVORITED... Success!!!');
+        }
+      });
+    }
+  });
+}
+
 retweetLatest();
+favoriteTweet();
